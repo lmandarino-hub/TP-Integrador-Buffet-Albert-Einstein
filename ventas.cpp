ef197d79448f2d1375937ea7607b494fcb585da1
AragonChristian20 <caragoncorrea@frba.utn.edu.ar>
2026-08-23 14:59:06 -0300

    guardando en rama

---
+++ ventas.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring> 
+using namespace std; 
+
+const float TASA_COMISION = 0.10f;
+
+struct ventas
+{ 
+    int ClaveProducto; 
+    float precio;
+};
+
+struct Mozo
+{
+    int idMozo;
+    char Nombre[50];
+    char PassWord[20];
+    float TotalCOMISION;
+};
+
+
