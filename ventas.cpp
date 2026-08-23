ad78a5ce89dd066a45d23f6bb91257e0f6a52efd
AragonChristian20 <caragoncorrea@frba.utn.edu.ar>
2026-08-23 15:53:27 -0300

    agregar a la rama

--- ventas.cpp
+++ ventas.cpp
@@ -18,3 +18,19 @@ struct Mozo
     char PassWord[20];
     float TotalCOMISION;
 };
+
+int main(){
+    string fecha; 
+    cout<<"ingrese la fecha de hoy (dd-mm-aaaa): "; 
+    cin>>fecha; 
+
+    string Fecha= "Comandas_"+fecha + ".dat"; 
+    fstream archivo(Fecha.c_str(),ios::in|ios::out|ios::binary); 
+
+    if (!archivo){
+        archivo.open(Fecha,ios::out|ios::binary);
+        archivo.close; 
+
+        archivo.open(Fecha,ios::in|ios::out|ios::binary);
+    }
+}
