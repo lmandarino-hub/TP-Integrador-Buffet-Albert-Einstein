52e53d66f4b842530d19fdc69d3b98c7baa3d602
sebasss22 <sebastianmore2311@gmail.com>
2026-08-28 11:54:09 -0300

    Modificando

--- cierre.cpp
+++ cierre.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+
 struct Comanda {
     int idMozo;
     int codigoProducto;
     int cantidad;
     float comision;
 };
+
 int main (){
+    int semana;
+    int mes;
+    int anio=2026;
+    cout << "Ingrese la semana (1-4):";
+    cin >> semana;
+    cout << "Ingrese el mes (1-12):";
+    cin >> mes;
 
 }
